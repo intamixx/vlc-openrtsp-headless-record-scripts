cedc4da702621876702e5c6fce65a0de7b5eaea0
intamixx <intamixx@hotmail.com>
2015-10-01 16:47:39 +0100

    add python inotify and launch script

---
+++ inotify-kam.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# Copyright (c) Intamixx
+
+"""
+Daemonize pyinotify's notifier for watching camera directories and launching record script
+"""
+
+# Import required libraries, raise an exception if not installed
+try:
+        import functools
+        import sys
+        import os
+        import pyinotify
+        import subprocess
+        import syslog
+        import time
+except ImportError as e:
+        print "\n%s is not installed. Please install it before running this script." % (e)
+        exit (1)
+
+cameras = ['front', 'tree', 'back']
+
+class EventHandler(pyinotify.ProcessEvent):
+        def process_IN_CREATE(self, event):
+                print "Creating:", event.pathname
+                for camera in cameras:
+                        if camera in event.pathname:
+                                syslog.syslog(syslog.LOG_INFO, "Running %s %s" % (camera, event.pathname) )
+                                runCMD('%s' % camera)
+
+        def process_IN_DELETE(self, event):
+                print "Removing:", event.pathname
+
+	def runCMD(camera):
+		cmd = "/usr/bin/python /home/vyos/scripts/pykam-rtsp-rec.py -c %s -d 32" % camera
+		syslog.syslog(syslog.LOG_INFO, "Checking CMD %s is running ..." % cmd)
+		process = os.popen("ps ax -o pid,cmd | grep -i \'%s\' | grep -v grep" % cmd).read()
+	
+		if (process):
+		        print process
+			process = process.lstrip(' ')
+		        process = process.rstrip()
+		        (pid, prog) = process.split(' ',1)
+		        syslog.syslog(syslog.LOG_INFO, "Program CMD %s [%s] is already running! ..." % (prog, pid) )
+		else:
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			syslog.syslog(syslog.LOG_INFO, "Launching CMD %s [%s]" % (cmd, p.pid) )
+
+wm = pyinotify.WatchManager()
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE # watched events
+
+handler = EventHandler()
+notifier = pyinotify.Notifier(wm, handler)
+
+dateNow = time.strftime("%Y%m%d")
+
+# Change watch directories here
+watch1 = "/mnt/deracam-front/%s/images" % dateNow
+watch2 = "/mnt/deracam-tree/%s/images" % dateNow
+watch3 = "/mnt/deracam-back/%s/images" % dateNow
+
+wm.add_watch([watch1, watch2, watch3], mask, rec=True)
+
+#on_loop_func = functools.partial(on_loop, counter=Counter() )
+try:
+        notifier.loop(daemonize=True, pid_file='/tmp/pykamnotify.pid', stdout='/tmp/pykamnotify.log', stderr='/tmp/pykamnotify.log')
+except pyinotify.NotifierError, err:
+        print >> sys.stderr, err
---
+++ pykam-rtsp.rec.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+
+# Copyright (c) Intamixx
+
+"""
+Custom record facility to record from rtsp cameras using ffmpeg.
+Kills ffmpeg using sigint after recording duration has lapsed.
+
+python kamrecord.py -c <camera: front, tree, back> -d <record duration in seconds>
+"""
+
+# Import required libraries, raise an exception if not installed
+try:
+        import syslog
+        import time
+        import getopt
+        import sys
+        import os
+        import socket
+except ImportError as e:
+        print "\n%s is not installed. Please install it before running this script." % (e)
+        exit (1)
+
+from os import kill
+from signal import alarm, signal, SIGALRM, SIGKILL, SIGINT
+from subprocess import PIPE, Popen
+
+def front():
+    hostip = '192.168.10.1'
+    port = '2091'
+    return hostip, port
+def tree():
+    hostip = '192.168.10.2'
+    port = '2092'
+    return hostip, port
+def back():
+    hostip = '192.168.10.3'
+    port = '2093'
+    return hostip, port
+def errhandler():
+    print "Unknown Camera, Run with -h for help"
+    sys.exit(1)
+
+cameras = { "front" : front,
+                "tree" : tree,
+                "back" : back,
+}
+
+def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1, env = None):
+    '''
+    Run a command with a timeout after which it will be forcibly
+    killed.
+    '''
+    class Alarm(Exception):
+        pass
+    def alarm_handler(signum, frame):
+        raise Alarm
+    p = Popen(args, shell = shell, cwd = cwd, stdout = PIPE, stderr = PIPE, env = env)
+    syslog.syslog(syslog.LOG_INFO, "Running %s -> Pid [%d] " % (ffmpeg, p.pid) )
+    if timeout != -1:
+        signal(SIGALRM, alarm_handler)
+        alarm(timeout)
+    try:
+        stdout, stderr = p.communicate()
+        if timeout != -1:
+            alarm(0)
+    except Alarm:
+        pids = [p.pid]
+        if kill_tree:
+            pids.extend(get_process_children(p.pid))
+        for pid in pids:
+            # process might have died before getting to this line
+            # so wrap to avoid OSError: no such process
+            syslog.syslog(syslog.LOG_INFO, "Killing %s -> Pid [%d] " % (ffmpeg, p.pid) )
+            try: 
+                kill(pid, SIGINT)
+            except OSError:
+                pass
+        return -2, '', ''
+    return p.returncode, stdout, stderr
+
+def get_process_children(pid):
+    p = Popen('ps --no-headers -o pid --ppid %d' % pid, shell = True,
+              stdout = PIPE, stderr = PIPE)
+    stdout, stderr = p.communicate()
+    return [int(p) for p in stdout.split()]
+
+def check_camera(hostip, port, camera):
+        # Create a TCP socket
+                s = socket.socket()
+                syslog.syslog(syslog.LOG_INFO, "Attempting connect -> %s Kam" % camera)
+                print "Attempting to connect to %s on port %s" % (hostip, port)
+                try:
+                        s.connect((hostip, port))
+                        syslog.syslog(syslog.LOG_INFO, "Connected -> %s:%s" % (hostip, port) )
+                        print "Connected to %s on port %s" % (hostip, port)
+                        return True
+                except socket.error, e:
+                        syslog.syslog(syslog.LOG_INFO, "Connect Failed -> %s:%s" % (hostip, port) )
+                        print "Connection to %s on port %s failed: %s" % (hostip, port, e)
+                        os._exit(1)
+                        return False
+
+def main(argv):
+    camera = ''
+    duration = ''
+    hostip = ''
+    port = ''
+    service = '12'
+    framerate = '25'
+    quality = '1'
+    username = 'user'
+    password = 'pass'
+    timeNow = time.strftime("%Y%m%d%H%M%S")
+    directory = '/mnt/videos'
+
+    try:
+        opts, args = getopt.getopt(argv,"hc:d:",["camera=","duration="])
+    except getopt.GetoptError:
+     print '%s -c <camera: front, tree, back> -d <record duration in seconds>' % format(os.path.abspath(__file__))
+     sys.exit(2)
+    for opt, arg in opts:
+     if opt == '-h':
+        print '%s -c <camera: front, tree, back> -d <record duration in seconds>' % format(os.path.abspath(__file__))
+        sys.exit()
+     elif opt in ("-c", "--camera"):
+        camera = arg
+     elif opt in ("-d", "--duration"):
+        duration = arg
+    if not camera or not duration:
+        print '%s -c <camera: front, tree, back> -d <record duration in seconds>' % format(os.path.abspath(__file__))
+        sys.exit(1)
+    else:
+        duration = int(duration)
+
+    # locate ffmpeg program
+    cmd_exists = lambda x: any(os.access(os.path.join(path, x), os.X_OK) for path in os.environ["PATH"].split(os.pathsep))
+    try:
+        result = cmd_exists('%s' % ffmpeg)
+    except e: sys.exit('ERROR: Error locating %s' % ffmpeg)
+    if result == False:
+        sys.exit('ERROR: Error %s not found' % ffmpeg)
+
+    # check directory exists
+    result = os.path.exists('%s' % directory)
+    if result == False:
+        syslog.syslog(syslog.LOG_INFO, "Directory %s not Found" % directory )
+        print 'Directory %s not Found' % (directory)
+        try:
+                 os.makedirs('%s' % directory)
+        except OSError:
+                syslog.syslog(syslog.LOG_INFO, "Could not create directory %s" % directory )
+                sys.exit('ERROR: Could not create directory %s' % directory)
+
+    ( hostip, port ) = cameras.get(camera, errhandler)()
+
+    print "Camera: %s\nIP: %s\nRTSP Port: %s" % (camera, hostip, port)
+
+    port = int(port)
+    check = check_camera(hostip, port, camera)
+
+    syslog.syslog(syslog.LOG_INFO, "Running %s -> Record %s Kam" % (ffmpeg, camera) )
+    runStr = "%s -rtsp_transport tcp -y -i rtsp://%s:%s@%s:%s/%s -r %s -an /%s/%s-%s.mp4" % (ffmpeg, username, password, hostip, port, service, framerate, directory, camera, timeNow)
+
+    print runStr
+    ( retCode, stdout, stderr ) = run(runStr, shell = True, timeout = duration)
+    retCode = int(retCode)
+    if (retCode == -2):
+        print "Command exited normally"
+        sys.exit(0)
+    else:
+        print "Command failed"
+        print "Return Code: %s" % retCode
+        print "%s\n%s" % (stdout, stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    ffmpeg = 'ffmpeg'
+    main(sys.argv[1:])
